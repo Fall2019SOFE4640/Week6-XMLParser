4a7f1a859b09436bd401c462374796d426889053
AnwarBari <33730047+AnwarBari@users.noreply.github.com>
2019-09-30 12:46:32 -0400

    first commit

--- app/src/main/java/com/example/xmlparser/XmlParser.java
+++ app/src/main/java/com/example/xmlparser/XmlParser.java
@@ -16,5 +16,7 @@ public class XmlParser extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_xml_parser);
+
+
     }
 }
