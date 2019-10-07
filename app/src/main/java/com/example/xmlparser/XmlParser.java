d0c6d47d9db5439adea903f9642d641f6a3796e9
AnwarBari <33730047+AnwarBari@users.noreply.github.com>
2019-10-07 13:58:51 -0400

    second commit

--- app/src/main/java/com/example/xmlparser/XmlParser.java
+++ app/src/main/java/com/example/xmlparser/XmlParser.java
@@ -29,7 +29,7 @@ public class XmlParser extends AppCompatActivity {
         setContentView(R.layout.activity_xml_parser);
 
 
-        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.linearLayoutParser);
+       //LinearLayout linearLayout = (LinearLayout) findViewById(R.id.linearLayoutParser);
         XmlResourceParser parser = getResources().getXml(R.xml.weather);
         //pass the parser to process it
         try {
@@ -44,6 +44,8 @@ public class XmlParser extends AppCompatActivity {
 
     private void processData(XmlResourceParser parser) throws IOException, XmlPullParserException {
         int eventType = -1;
+
+
         while (eventType != XmlResourceParser.END_DOCUMENT){//1
          if(eventType == XmlResourceParser.START_TAG){//2
           String localName =  parser.getName();
@@ -52,16 +54,47 @@ public class XmlParser extends AppCompatActivity {
               String temperature = parser.getAttributeValue(null,"temperature");
               String weather = parser.getAttributeValue(null,"weather");
               Log.d("testParser",city+ " "+ temperature+ " "+ weather );
-              printValues(city,temperature,weather);
-
-          }
+              printValues(city,temperature,weather);}
           }
             eventType=parser.next();
          }
         }
 
     private void printValues(String city, String temperature, String weather) {
+        LinearLayout weatherData = (LinearLayout) new LinearLayout(this);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(0,ViewGroup.LayoutParams.WRAP_CONTENT);
 
+        params.weight=1;
+        TextView cityText = new TextView(this);
+
+        TextView TemperatureText = new TextView(this);
+        TextView weatherText = new TextView(this);
+
+
+        LinearLayout linearLayout =findViewById(R.id.linearLayoutParser);
+
+        cityText.setLayoutParams(params);
+        cityText.setText(city);
+        cityText.setTextSize(24);
+        cityText.setTextColor(Color.RED);
+        cityText.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+        weatherData.addView(cityText);
+
+        TemperatureText.setLayoutParams(params);
+        TemperatureText.setText(temperature);
+        TemperatureText.setTextSize(24);
+        TemperatureText.setTextColor(Color.RED);
+        TemperatureText.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+        weatherData.addView(TemperatureText);
+
+        weatherText.setLayoutParams(params);
+        weatherText.setText(weather);
+        weatherText.setTextSize(24);
+        weatherText.setTextColor(Color.RED);
+        weatherText.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+        weatherData.addView(weatherText);
+
+        linearLayout.addView(weatherData);
 
     }
 
